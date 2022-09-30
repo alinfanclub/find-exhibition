71875450e1997c32899269cddc019e9cdef5b201
e759ksh <e759ksh@naver.com>
2022-09-30 09:56:12 +0900

    로그인 기능 추가 버그 고쳐야함

--- back/routes/api/setMarker.js
+++ back/routes/api/setMarker.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const multer = require('multer');
-const mongoose = require('mongoose');
 
 
 const storage = multer.diskStorage({
--- back/server.js
+++ back/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const chalk = require('chalk');
 
+
+
 const app = express();
 
 //Middle ware
@@ -10,9 +12,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const markers = require('./routes/api/setMarker');
+const userRoutes = require('./routes/api/user');
 
 app.use('/api/space', markers);
-
+app.use('/user', userRoutes);
 
 const port = process.env.PORT || 5001;
 
